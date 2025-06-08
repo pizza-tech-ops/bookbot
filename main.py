763965a1551e8f98776e32c3ead4ed7c6f615cb8
pizza-tech-ops <sneakyllama@protonmail.com>
2025-06-08 22:40:14 +0900

    add count_words to tally frankenstein words

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
+def count_words(text):
+    return len(text.split())
+
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    num_words = count_words(book_text)
+    print(f"{num_words} words found in the document")
 
 if __name__ == "__main__":
     main()
