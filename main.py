044acb34f1a94a8afcfbcd6f0154afaa35ad7293
pizza-tech-ops <sneakyllama@protonmail.com>
2025-06-08 23:28:43 +0900

    refractor count_words to stats.py module

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+from stats import count_words
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-
-def count_words(text):
-    return len(text.split())
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
